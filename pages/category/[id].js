c9d0e45b38c227f24577706b0c1efb59f937100d
omsdev <orangemegasoftwaredev@gmail.com>
2024-01-10 12:25:11 +0530

    responsive

--- pages/category/[id].js
+++ pages/category/[id].js
@@ -6,6 +6,7 @@ import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import { cetegoryByproducts } from "../../utils/api";
 import { useToasts } from "react-toast-notifications";
+import { LazyLoadImage } from "react-lazy-load-image-component";
 
 const ShopGrid = () => {
   const router = useRouter();
@@ -28,7 +29,7 @@ const ShopGrid = () => {
   }, [id]);
 
   return (
-    <Layout>
+    <Layout footer={3}>
       <PageBanner pageName={"Products"} />
 
       <section className="shop-page rel z-1 pt-120 rpt-90 pb-130 rpb-100">
@@ -39,55 +40,55 @@ const ShopGrid = () => {
 
           <div className="row show-grid-row">
             {products.length > 0 ? (
-              <>
-                {products.map((product) => (
-                  <Link href={`/product-details/${product.id}`}>
-                    <div
-                      key={product.id}
-                      className="col-xl-4 col-lg-4 col-md-6 col-sm-6 col-6 item fruits bread"
-                    >
-                      <div className="product-item wow fadeInUp delay-0-2s">
-                        {product.offer && (
-                          <span className="offer">{product.offer}</span>
-                        )}
-                        <div className="image">
-                          <img
-                            src={product.image}
-                            alt={product.name}
-                            style={{
-                              height: "185px",
-                              width: "100%",
-                              objectFit: "contain",
-                            }}
-                          />{" "}
-                        </div>
-                        <div className="content">
-                          <div className="ratting">
-                            {/* {[...Array(product.rating)].map((_, index) => (
-                    <i key={index} className="fas fa-star" />
-                  ))} */}
-                          </div>
-                          <h5>
-                            <Link href={`/product-details/${product.id}`}>
-                              {product.name}
-                            </Link>
-                          </h5>
-                          {product.discountedPrice ? (
-                            <span className="price">
-                              <del>{product.originalPrice}</del>
-                              <span>{product.discountedPrice}</span>
-                            </span>
-                          ) : (
-                            <span className="price">
-                              <span>{product.price}</span>
-                            </span>
-                          )}
-                        </div>
-                      </div>
-                    </div>
-                  </Link>
-                ))}
-              </>
+                   <>
+                   {products.map((product) => (
+                     <div
+                       key={product.id}
+                       className="col-xl-4 col-lg-4 col-md-6 col-sm-6 col-6 item fruits bread"
+                       style={{ display: "flex", flexDirection: "column" }}
+                     >
+                       <div
+                         className="product-item wow fadeInUp delay-0-2s"
+                         style={{ flex: "1" }}
+                       >
+                         {/* Displaying discount offer if available */}
+                         <span className="offer">50% Off</span>
+                         <div className="image">
+                           {/* Assuming your image path is static/assets/images/products/ */}
+                           <LazyLoadImage
+                             src={product.image}
+                             alt={product.name}
+                             effect="blur" // Optional: Add a blur effect while loading
+                             threshold={200} // Optional: Adjust the threshold for when the image starts loading
+                             style={{
+                               height: "185px",
+                               width: "100%",
+                               objectFit: "contain",
+                             }}
+                           />
+                         </div>
+                         <div className="content">
+                           <div className="ratting">
+                             {/* Displaying star rating */}
+                             {Array.from({ length: 5 }).map((_, index) => (
+                               <i key={index} className="fas fa-star" />
+                             ))}
+                           </div>
+                           <h5 style={{ height: "50px", overflow: "hidden" }}>
+                             {/* Linking to product details page */}
+                             <Link href={`/product-details/${product.id}`}>
+                               {product.name}
+                             </Link>
+                           </h5>
+                           <span className="price">
+                             {/* Displaying original and discounted prices */}
+                             {product.price && <span>{product.price}</span>}
+                           </span>
+                         </div>
+                       </div>
+                     </div>
+                   ))}
+                 </>
             ) : (
               <center>No data found </center>
             )}
